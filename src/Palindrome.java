8af999a000cbbae3581694059c9dcddfa8a20906
spookyeugene <48047078+spookyeugene@users.noreply.github.com>
2020-04-12 13:10:03 +0300

    Lab1

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 public class Palindrome {
+
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in); //считывание вводимых с клавиатуры значений
     String input_string = in.next();
     System.out.println(isPalindrome(input_string));//вызов функции и вывод результатов
   }
-  public static boolean isPalindrome(String s){ //функция, показывающая является ли строка палиндромом
-    if (s.equals(reverseString(s))){ //проверка, равна ли введенная строка отображенной
+
+  public static boolean isPalindrome(String s) { //функция, показывающая является ли строка палиндромом
+    if (s.equals(reverseString(s))) { //проверка, равна ли введенная строка отображенной
       return true;
     } else {
       return false;
     }
   }
+
   public static String reverseString(String any_str) { //функция, отображающая введенные
     String default_str = "";                           //данные в обратном порядке
-    for (int litter_number = any_str.length(); litter_number>0; litter_number--){
-      default_str+=any_str.charAt(litter_number-1);
+    for (int litter_number = any_str.length(); litter_number > 0; litter_number--) {
+      default_str += any_str.charAt(litter_number - 1);
     }
     return default_str;
   }
 }
+
